94f91cf1efea7243b94b45da9d3b3eb59123ddda
tutunak <dk@tutunak.com>
2023-09-02 20:18:51 +0400

    init checker

---
+++ feedcheck.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func lastModified(url string) (string, error) {
+	// Check the Last-Modified header
+	resp, err := http.Head(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+	return resp.Header.Get("Last-Modified"), nil
+}
+func main() {
+	feed := os.Getenv("RSS_FEED")
+	if feed == "" {
+		log.Fatal("RSS_FEED environment variable is not set")
+	}
+	modificationTime, err := lastModified(feed)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if modificationTime != "" {
+		log.Printf("Last-Modified: %s", modificationTime)
+	}
+}
